b4e77d9735fd822de346d44f3be8bd93546946b8
Patrick Vasconcelos <patrickvs@hotmail.com>
2020-11-22 02:59:30 -0300

    [NEW] Rotas e animação no toast message

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { ToastMessage, useToast } from '../../hooks/toast';
+import { useTransition } from 'react-spring';
+import { ToastMessage } from '../../hooks/toast';
 import { Container } from './styles';
 import Toast from './Toast';
 
@@ -7,12 +8,23 @@ interface ToastContainerProps {
   messages: ToastMessage[];
 }
 
-const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => (
-  <Container>
-    {messages.map(message => (
-      <Toast key={message.id} message={message} />
-    ))}
-  </Container>
-);
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+  const messagesWithTransactions = useTransition(
+    messages,
+    message => message.id,
+    {
+      from: { right: '-120%', opacity: 0 },
+      enter: { right: '0%', opacity: 1 },
+      leave: { right: '-120%', opacity: 0 },
+    },
+  );
 
+  return (
+    <Container>
+      {messagesWithTransactions.map(({ item, key, props }) => (
+        <Toast key={key} style={props} message={item} />
+      ))}
+    </Container>
+  );
+};
 export default ToastContainer;
