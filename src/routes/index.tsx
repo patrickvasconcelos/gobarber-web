6f3020a06819c723283a6e109bace3d5c1f1f725
Patrick Vasconcelos <patrickvs@hotmail.com>
2020-11-24 02:13:22 -0300

    [NEW] Rotas autenticadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
+import Dashboard from '../pages/Dashboard';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
@@ -8,6 +10,7 @@ const Routes: React.FC = () => {
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/signup" exact component={SignUp} />
+      <Route path="/dashboard" exact component={Dashboard} isPrivate />
     </Switch>
   );
 };
