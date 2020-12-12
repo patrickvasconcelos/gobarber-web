9acd001762587f60d2303b52f64cbb2f6d159428
Patrick Vasconcelos <patrickvs@hotmail.com>
2020-12-12 17:58:09 -0300

    [NEW] Listagem de agendamentos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import React, { useState } from 'react';
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Appointment,
+  Calendar,
+  Container,
+  Content,
+  Header,
+  HeaderContent,
+  NextAppointment,
+  Profile,
+  Schedule,
+  Section,
+} from './styles';
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectDate, setSelectDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -13,7 +26,10 @@ const Dashboard: React.FC = () => {
         <HeaderContent>
           <img src={logoImg} alt="GoBarber" />
           <Profile>
-            <img src={user.avatar_url} alt={user.name} />
+            <img
+              src="https://avatars1.githubusercontent.com/u/45183557?s=460&u=dd0f43cfb655463b1dac13700a5df8860b065e4d&v=4"
+              alt={user.name}
+            />
             <div>
               <span>Bem vindo,</span>
               <strong>{user.name}</strong>
@@ -25,6 +41,80 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 6</span>
+            <span>Segunda-Feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars1.githubusercontent.com/u/45183557?s=460&u=dd0f43cfb655463b1dac13700a5df8860b065e4d&v=4"
+                alt="Patrick Vasconcelos"
+              />
+              <strong>Patrick Vasconcelos</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/45183557?s=460&u=dd0f43cfb655463b1dac13700a5df8860b065e4d&v=4"
+                  alt="Patrick Vasconcelos"
+                />
+                <strong>Patrick Vasconcelos</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/45183557?s=460&u=dd0f43cfb655463b1dac13700a5df8860b065e4d&v=4"
+                  alt="Patrick Vasconcelos"
+                />
+                <strong>Patrick Vasconcelos</strong>
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/45183557?s=460&u=dd0f43cfb655463b1dac13700a5df8860b065e4d&v=4"
+                  alt="Patrick Vasconcelos"
+                />
+                <strong>Patrick Vasconcelos</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
