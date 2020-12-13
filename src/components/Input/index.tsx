39f222b665f3cbb90387713c075f5a3b8fde8bbf
Patrick Vasconcelos <patrickvs@hotmail.com>
2020-12-13 12:25:29 -0300

    [NEW] página de edição do perfil

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -12,10 +12,16 @@ import { Container, Error } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
+  containerStyle?: object;
   icon: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+const Input: React.FC<InputProps> = ({
+  name,
+  containerStyle,
+  icon: Icon,
+  ...rest
+}) => {
   const inputRef = useRef<HTMLInputElement>(null);
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -36,7 +42,12 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
     registerField({ name: fieldName, ref: inputRef.current, path: 'value' });
   }, [fieldName, registerField]);
   return (
-    <Container isErrored={!!error} isFilled={isFilled} isFocused={isFocused}>
+    <Container
+      style={containerStyle}
+      isErrored={!!error}
+      isFilled={isFilled}
+      isFocused={isFocused}
+    >
       {Icon && <Icon />}
       <input
         onFocus={handleInputFocus}
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -5,6 +5,7 @@ import 'react-day-picker/lib/style.css';
 import { format, isAfter, isToday, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
+import { Link } from 'react-router-dom';
 import {
   Appointment,
   Calendar,
@@ -141,7 +142,9 @@ const Dashboard: React.FC = () => {
             <img src={user.avatar_url} alt={user.name} />
             <div>
               <span>Bem vindo,</span>
-              <strong>{user.name}</strong>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
